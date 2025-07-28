83d15e13a7de033dc5e1d00cc0f73d5bad3390e1
Loago <bida24-067@thuto.bac.ac.bw>
2025-07-28 09:09:59 +0000

    edited

--- animation.js
+++
@@ -1,8 +0,0 @@
-// Image animation on page load
-  window.addEventListener('DOMContentLoaded', () => {
-    const kayaImg = document.getElementById('KayaIN');
-    kayaImg.classList.add('expanded');
-    setTimeout(() => {
-      kayaImg.classList.remove('expanded');
-    }, 3000);
-  });
